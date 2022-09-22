58fc736f6b0385822aeb63648c6dce8db1b28a1a
barreto1060 <trifon.barreto@uab.edu.bo>
2022-09-21 21:32:29 -0400

    Segunda Modificacion

--- js/script.js
+++ js/script.js
@@ -19,85 +19,84 @@ const $btnSignIn = document.querySelector(".sign-in-btn"),
   $signUpi = document.getElementById("sign-upi"),
   $signUni = document.getElementById("sign-uni"),
   $texto = document.getElementById("txtnom"),
+  $nombre = document.getElementById("nombre"),
   $signIn = document.querySelector(".sign-in");
 
 document.addEventListener("click", (e) => {
   if (e.target === $btnSignIn || e.target === $btnSignUp) {
     $signIn.classList.toggle("active");
     $signUp.classList.toggle("active");
-
     if (e.target === $btnSignIn && $texto.value !== "") {
       $signUpi.classList.add("active");
       $signUni.classList.add("active");
-      $btnSignIni.innerHTML = "<i class='fa fa-refresh' aria-hidden='true'></i> Reiniciar sesion";
+      $btnSignIni.innerHTML =
+        "<i class='fa fa-refresh' aria-hidden='true'></i> Reiniciar sesion";
       let usuario = $texto.value;
-      sesion(usuario);
+      $nombre.innerHTML =
+        "Bienvenido  " + usuario + "   <i class='fas fa-user'></i>";
     } else {
       $signUpi.classList.remove("active");
       $signUni.classList.remove("active");
     }
   }
 });
-const sesion = (usuario) => {
-  let usu = (document.getElementById("nombre").innerHTML =
-    "Bienvenido  " + usuario + "   <i class='fas fa-user'></i>");
-  // element.innerHTML = usu;
-};
 
-let x = document.getElementById("mensage");
-let y = document.getElementById("conversor");
-let z = document.getElementById("elegir");
+let mensagex = document.getElementById("mensage");
+let conversory = document.getElementById("conversor");
+let elegirz = document.getElementById("elegir");
 let btnreg = document.getElementById("btnreg");
 let btnmon = document.getElementById("btnmon");
 const registro = () => {
-  x.style.left = "0px";
-  y.style.left = "950px";
-  z.style.left = "0px";
+  mensagex.style.left = "0px";
+  conversory.style.left = "950px";
+  elegirz.style.left = "0px";
   btnreg.classList.add("miBoton");
   btnreg.classList.remove("miBotonNul");
   btnmon.classList.add("miBotonNul");
 };
 const moneda = () => {
-  x.style.left = "-950px";
-  y.style.left = "0px";
-  y.style.width = "100%";
-  z.style.left = "0px";
+  mensagex.style.left = "-950px";
+  conversory.style.left = "0px";
+  conversory.style.width = "100%";
+  elegirz.style.left = "0px";
   btnreg.classList.add("miBotonNul");
   btnreg.classList.remove("miBoton");
   btnmon.classList.remove("miBotonNul");
   btnmon.classList.add("miBoton");
 };
 
-y.addEventListener("submit", (e) => {
+conversory.addEventListener("submit", (e) => {
   e.preventDefault();
   cambioMo();
 });
 const cambioMo = () => {
-  let valor = parseInt(document.getElementById("valorMo").value);
-
-  let resultado = 0;
-  let mdolar = 6.91,
+  let valor = parseInt(document.getElementById("valorMo").value),
+    dolar = document.getElementById("dolar"),
+    euro = document.getElementById("euro"),
+    real = document.getElementById("real"),
+    cantidad = document.getElementById("cantidad"),
+    resultado = 0,
+    mdolar = 6.96,
     meuro = 10.5,
     mreal = 2.5;
 
-  if (document.getElementById("dolar").checked) {
+  if (dolar.checked) {
     resultado = valor * mdolar;
 
-    document.getElementById("cantidad").innerHTML = "  " + resultado.toFixed(2)+"  Bs";
-  } else if (document.getElementById("euro").checked) {
+    cantidad.innerHTML = "  " + resultado.toFixed(2) + "  Bs";
+  } else if (euro.checked) {
     resultado = valor * meuro;
-    document.getElementById("cantidad").innerHTML = "  " + resultado.toFixed(2)+"  Bs";
-  } else if (document.getElementById("real").checked) {
+    cantidad.innerHTML = "  " + resultado.toFixed(2) + "  Bs";
+  } else if (real.checked) {
     resultado = valor * mreal;
-    document.getElementById("cantidad").innerHTML = "  " + resultado.toFixed(2)+"  Bs";
+    cantidad.innerHTML = "  " + resultado.toFixed(2) + "  Bs";
   } else {
-    alert("fd");
+    alert("Ingrese algun dato");
   }
 };
 
 const radioB = () => {
   let tMo = document.getElementById("tipoM");
-
   if (document.getElementById("dolar").checked) {
     tMo.innerHTML = "en Dolares";
   } else if (document.getElementById("euro").checked) {
@@ -105,6 +104,6 @@ const radioB = () => {
   } else if (document.getElementById("real").checked) {
     tMo.innerHTML = "en Reales";
   } else {
-    alert("fd");
+    alert("Ingrese algun dato");
   }
 };
